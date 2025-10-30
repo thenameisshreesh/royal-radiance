195198f9328aba9439f6eceecc3e9c28fb784ea3
thenameisshreesh <shreeshpitambare084@gmail.com>
2025-10-30 10:32:41 +0530

    Prepared for Vercel deployment

--- app.py
+++ app.py
@@ -232,7 +232,4 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.secret_key = app.config['SECRET_KEY']
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+app = create_app()
