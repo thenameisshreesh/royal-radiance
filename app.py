3b1775056782dd6db5e0053b75101743cde07537
thenameisshreesh <shreeshpitambare084@gmail.com>
2025-11-11 15:41:05 +0530

    update

--- app.py
+++ app.py
@@ -199,5 +199,9 @@ def create_app():
 
 
 app = create_app()
-if __name__ == "__main__":
-    app.run(debug=True)
+# vercel entry point
+app = Flask(__name__)
+
+# your routes here...
+
+# don't call app.run()
